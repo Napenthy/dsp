a054c24263f34d60c83782c04ec9637e0f6e569f
Napenthy <bncrane@me.com>
2016-03-24 13:53:35 -0400

    Complete q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -7,13 +7,31 @@
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
 
-import csv
+# import pandas
+import pandas as pd
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+# read football.csv data
+data = pd.read_csv('football.csv')
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+# get min score difference between 'Goals' and 'Goals Allowed'
+# by creating adding a new column "Goals Difference' equalling
+# the absolute value of the difference between 'Goals' and 'Goals Allowed' 
+data['Goals Difference'] = abs(data['Goals'] - data['Goals Allowed'])
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+# determine minimum number of 'Goals Difference'
+min_num_goals = data['Goals Difference'].min()
+
+# Check for multiple 'Teams' with lowest 'Goals Difference'
+count = 0 
+for i in data['Goals Difference']:
+    if i == min_num_goals:
+        count += 1
+# print result
+if count > 1:
+    print str(count) + " teams share the smallest difference in 'for' and 'against' goals:"
+    print data.ix[data['Goals Difference']==1]['Team']
+else:
+    print data.iloc[data['Goals Difference'].idxmin()]["Team"] + " has the smallest difference in 'for' and 'against' goals, with " + str(min_num_goals)
+
+
+
