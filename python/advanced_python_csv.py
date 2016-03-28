5df46a528b82039e62f579b58db26fcb715755e2
Napenthy <bncrane@me.com>
2016-03-28 12:47:24 -0400

    completed advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,10 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv', 'rb') as faculty:
+    with open("emails.csv", 'wb') as f:
+        reader = csv.reader(faculty)
+        writer = csv.writer(f)
+        next(faculty)
+        for name, degree, title, email in reader:
+            writer.writerow([email])
+
