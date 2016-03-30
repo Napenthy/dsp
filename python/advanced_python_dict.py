cb81cb3e83b3343073569f86ebd5c40331e4edc2
Napenthy <bncrane@me.com>
2016-03-29 20:51:26 -0400

    complete advanced_python_dict.py
     @reshama @aramnath

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,37 @@
-PLACE YOUR CODE HERE
+import csv
+from operator import itemgetter
+
+faculty_dict = {}
+professor_dict = {}
+
+with open('faculty.csv', 'rb') as f:
+    faculty = csv.reader(f)
+#    header = faculty.next()
+    for name, degree, title, email in faculty:
+        first = name.split()[0]
+        last = name.split()[-1]
+        faculty_dict[last] = [degree, title, email]
+        professor_dict[first, last] = [degree, title, email]  
+        
+#Q6.
+print
+count = 0
+for i in faculty_dict:
+    count += 1
+    if count < 4:
+        print str(i)+': '+str(faculty_dict[i])
+print
+#Q7.
+count = 0
+for i in professor_dict:
+    count += 1
+    if count < 4:
+        print str(i)+': '+str(professor_dict[i])
+print
+#Q8. 
+count = 0
+for k in sorted(professor_dict.keys(), key=itemgetter(1)):
+    count += 1
+    if count < 4:
+        print str(k)+': '+str(professor_dict[k])
+ 
